7ba8797455b7c9f51378dc94ef5f9ac2d857d5b2
Syun Sugiyama <bcss1220134@gn.iwasaki.ac.jp>
2013-01-18 10:07:17 +0900

    基本プログラミングA課題1

---
+++ CL1_K01_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*ì¬Ò@1T2(13) Sugiyama@Syun*/
+/*‰Û‘è–¼@‰Û‘è01_1*/
+
+void main(void) {
+	int sum;
+	int num1, num2;
+	num1 = 15;
+	num2 = 37;
+    sum = num1 + num2;
+	printf("num1+num2=%d‚Å‚·\n", sum);
+}
