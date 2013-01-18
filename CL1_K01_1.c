90d4cd8fc8b29d2137930f01e252ac39ffcd0ecb
Syun Sugiyama <bcss1220134@gn.iwasaki.ac.jp>
2013-01-18 10:12:11 +0900

    基本プログラミングA課題1
    sumからxに変更

--- CL1_K01_1.c
+++ CL1_K01_1.c
@@ -4,10 +4,10 @@
 /*‰Û‘è–¼@‰Û‘è01_1*/
 
 void main(void) {
-	int sum;
+	int x;
 	int num1, num2;
 	num1 = 15;
 	num2 = 37;
-    sum = num1 + num2;
-	printf("num1+num2=%d‚Å‚·\n", sum);
+    x = num1 + num2;
+	printf("num1+num2=%d‚Å‚·\n", x);
 }
